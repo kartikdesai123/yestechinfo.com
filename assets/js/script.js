890f02e74b8b38e7a82a20f3ccabf41536bd2a7d
unknown <kartikdesai123@gmail.com>
2020-03-30 12:41:29 +0530

    setup

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// (function() {
+    
+//   function controls() {
+//     $(this).addClass('selected').siblings('button').removeClass('selected');
+//     $('.slideDiv').animate({
+//       top: -$('.container2').height() * $(this).index()
+//     }, 450);
+//   }
+  
+//   /***Event Listeners***/
+//   const runCode = () => {
+//     const button = document.querySelectorAll('.button');
+//     if ( button ) {
+//       for ( var i = 0; i < button.length; i++ ) {
+//         button[i].addEventListener('click', controls, false);
+//       }
+//     }
+//   }
+  
+//   runCode();
+  
+// })();
+
+
